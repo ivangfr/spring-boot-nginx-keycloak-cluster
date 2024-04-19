a12b314c76b7d69a314bbe2647ed44c12e28e31a
Ivan Franchin <ivan.franchin@takeaway.com>
2024-04-19 23:09:27 +0200

    Fix identation in init-environment.sh

--- init-environment.sh
+++ init-environment.sh
@@ -20,51 +20,51 @@ echo "Starting postgres"
 echo "-----------------"
 
 docker run -d \
-    --name postgres \
-    -p 5432:5432 \
-    -e POSTGRES_DB=keycloak \
-    -e POSTGRES_USER=keycloak \
-    -e POSTGRES_PASSWORD=password \
-    --network=spring-boot-nginx-keycloak-cluster-net \
-    postgres:${POSTGRES_VERSION}
+  --name postgres \
+  -p 5432:5432 \
+  -e POSTGRES_DB=keycloak \
+  -e POSTGRES_USER=keycloak \
+  -e POSTGRES_PASSWORD=password \
+  --network=spring-boot-nginx-keycloak-cluster-net \
+  postgres:${POSTGRES_VERSION}
 
 echo
 echo "Starting keycloak 1"
 echo "-------------------"
 
 docker run -d \
-    --name keycloak1 \
-    -e KEYCLOAK_ADMIN=admin \
-    -e KEYCLOAK_ADMIN_PASSWORD=admin \
-    -e KC_DB=postgres \
-    -e KC_DB_URL_HOST=postgres \
-    -e KC_DB_URL_DATABASE=keycloak \
-    -e KC_DB_USERNAME=keycloak \
-    -e KC_DB_PASSWORD=password \
-    -e KC_CACHE=ispn \
-    -e KC_HOSTNAME=nginx.keycloak.cluster \
-    -e KC_LOG_LEVEL=INFO,org.infinispan:DEBUG,org.keycloak.events:DEBUG \
-    --network=spring-boot-nginx-keycloak-cluster-net \
-    quay.io/keycloak/keycloak:${KEYCLOAK_VERSION} start-dev
+  --name keycloak1 \
+  -e KEYCLOAK_ADMIN=admin \
+  -e KEYCLOAK_ADMIN_PASSWORD=admin \
+  -e KC_DB=postgres \
+  -e KC_DB_URL_HOST=postgres \
+  -e KC_DB_URL_DATABASE=keycloak \
+  -e KC_DB_USERNAME=keycloak \
+  -e KC_DB_PASSWORD=password \
+  -e KC_CACHE=ispn \
+  -e KC_HOSTNAME=nginx.keycloak.cluster \
+  -e KC_LOG_LEVEL=INFO,org.infinispan:DEBUG,org.keycloak.events:DEBUG \
+  --network=spring-boot-nginx-keycloak-cluster-net \
+  quay.io/keycloak/keycloak:${KEYCLOAK_VERSION} start-dev
 
 echo
 echo "Starting keycloak 2"
 echo "-------------------"
 
 docker run -d \
-    --name keycloak2 \
-    -e KEYCLOAK_ADMIN=admin \
-    -e KEYCLOAK_ADMIN_PASSWORD=admin \
-    -e KC_DB=postgres \
-    -e KC_DB_URL_HOST=postgres \
-    -e KC_DB_URL_DATABASE=keycloak \
-    -e KC_DB_USERNAME=keycloak \
-    -e KC_DB_PASSWORD=password \
-    -e KC_CACHE=ispn \
-    -e KC_HOSTNAME=nginx.keycloak.cluster \
-    -e KC_LOG_LEVEL=INFO,org.infinispan:DEBUG,org.keycloak.events:DEBUG \
-    --network=spring-boot-nginx-keycloak-cluster-net \
-    quay.io/keycloak/keycloak:${KEYCLOAK_VERSION} start-dev
+  --name keycloak2 \
+  -e KEYCLOAK_ADMIN=admin \
+  -e KEYCLOAK_ADMIN_PASSWORD=admin \
+  -e KC_DB=postgres \
+  -e KC_DB_URL_HOST=postgres \
+  -e KC_DB_URL_DATABASE=keycloak \
+  -e KC_DB_USERNAME=keycloak \
+  -e KC_DB_PASSWORD=password \
+  -e KC_CACHE=ispn \
+  -e KC_HOSTNAME=nginx.keycloak.cluster \
+  -e KC_LOG_LEVEL=INFO,org.infinispan:DEBUG,org.keycloak.events:DEBUG \
+  --network=spring-boot-nginx-keycloak-cluster-net \
+  quay.io/keycloak/keycloak:${KEYCLOAK_VERSION} start-dev
 
 echo
 wait_for_container_log "postgres" "database system is ready"
